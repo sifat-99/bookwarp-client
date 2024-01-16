aa7447167eba50cdb3cf4a59eb6e975621fc9053
dronatoo <mrdronatoo@gmail.com>
2024-01-16 08:45:21 -0800

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ContactUs from './contactUs/ContactUs'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-center my-5'>Home section</h1>
+      <h1 className='text-center my-5'>Home section.</h1>
       <ContactUs/>
     </main>
   )
