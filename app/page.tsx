7f91972b575aa186a4e8b55213b4c918dd2a4dc3
Shorif <shorifahamed571@gmail.com>
2024-01-18 20:09:25 +0600

    Footer Section Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./banner/Banner";
 import ContactUs from "./contactUs/ContactUs";
+import Footer from "./footer/Footer";
 export default function Home() {
   return (
     <main>
       <Banner />
       <h1 className="font-bold text-3xl text-center mt-14 mb-2">Contact Us</h1>
       <ContactUs />
+      <Footer />
     </main>
   );
 }
