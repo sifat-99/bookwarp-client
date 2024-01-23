7db400494b8994076a1e411a0717607668185cb5
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2024-01-23 22:05:34 +0600

    Google Login system added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,35 +1,36 @@
+import { signInWithPopup } from 'firebase/auth';
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
-import {signInWithEmailAndPassword} from 'firebase/auth';
+import GoogleProvider from "next-auth/providers/google";
+import { GoogleAuthProvider, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from "@/config/firebase";
 
-
-
 export const authOptions = {
-  // Configure one or more authentication providers
-  pages: {
-    signIn: '/login'
-  },
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {},
-      async authorize(credentials): Promise<any> {
-        return await signInWithEmailAndPassword(auth, (credentials as any).email || '', (credentials as any).password || '')
-          .then(userCredential => {
-            if (userCredential.user) {
-              return userCredential.user;
+    // Configure one or more authentication providers
+    pages: {
+        signIn: '/login'
+    },
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {},
+            async authorize(credentials): Promise<any> {
+                return await signInWithEmailAndPassword(auth, (credentials as any).email || '', (credentials as any).password || '')
+                    .then(userCredential => {
+                        if (userCredential.user) {
+                            return userCredential.user;
+                        }
+                        return null;
+                    })
+                    .catch(error => console.log(error));
             }
-            return null;
-          })
-          .catch(error => (console.log(error)))
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(error);
-  });
-      }
-    })
-  ],
+        }),
+        GoogleProvider({
+            name: 'Google',
+            clientId: '1051544115028-gn45vkok60a7ejfdiosm2dbd8s2fk3hi.apps.googleusercontent.com',
+            clientSecret: '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',
+        }),
+    ],
 }
+
 export default NextAuth(authOptions)
