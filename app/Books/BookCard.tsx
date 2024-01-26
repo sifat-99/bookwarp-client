f1692e19031d29b7c11c21c38f5247164989d336
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2024-01-26 18:15:52 +0600

    updated allBooks section and Navbar

--- app/Books/BookCard.tsx
+++ app/Books/BookCard.tsx
@@ -1,43 +1,56 @@
 import axios from "axios";
-import { url } from "inspector";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
-import { toast } from "react-toastify";
 import Swal from "sweetalert2";
+import { BsBookmarkStarFill } from "react-icons/bs";
 
 const BookCard = (props: any) => {
+  const { cover, title, writer, price } = props.books;
+  const userInfo = useSession();
+  const email = userInfo?.data?.user?.email;
 
-  const {cover, title, writer, price} = props.books;
-  const userInfo=useSession();
- const email=userInfo?.data?.user?.email;
-
-  const heandelBookmark=(cover: any, title: any, writer: any, price: any)=>{
-      const bookMark ={cover, title, writer, price, email};
-      if(email){
-        axios.post("https://bookwarp-server.vercel.app/bookmark", bookMark)
-        .then(res=>{
-          if(res.data.insertedId){
+  const heandelBookmark = (cover: any, title: any, writer: any, price: any) => {
+    const bookMark = { cover, title, writer, price, email };
+    if (email) {
+      axios
+        .post("https://bookwarp-server.vercel.app/bookmark", bookMark)
+        .then((res) => {
+          if (res.data.insertedId) {
             Swal.fire({
               position: "center",
               icon: "success",
               title: "Successfully sent message",
               showConfirmButton: false,
-              timer: 1500
-            })
+              timer: 1500,
+            });
           }
-        })
-      }
-  }
+        });
+    }
+  };
   return (
     <div>
       <div className="card h-[550px] card-compact bg-slate-200 pt-4 dark:bg-black dark:border shadow-xl p-2">
-              <div style={{backgroundImage: `url(${cover})`, background: "cover" }} className="w-[250px] h-[350px]"></div>
-              <div className="card-body">
-                <h2 className="text-xl text-black dark:text-white text-center font-semibold">{title}</h2>
-                <p className='text-center text-black dark:text-white'>{writer}</p>
-                <p className='text-center text-black dark:text-white'><span className='font-semibold'>Price:</span> {price}$</p>
-          <div className="card-actions justify-center">
+        <div
+          style={{ backgroundImage: `url(${cover})`, backgroundSize: "cover" }}
+          className="w-[250px] h-[350px] mx-auto rounded-lg"
+        ></div>
+        <div className="card-body">
+          <div className="flex justify-center items-center">
+            <h2 className="text-xl text-black dark:text-white text-center font-semibold">
+              {title}
+            </h2>
+            <button
+              onClick={() => heandelBookmark(cover, title, writer, price)}
+              className="btn bg-transparent border-none ml-8"
+            >
+              <BsBookmarkStarFill className="text-xl text-yellow-500" />
+            </button>
+          </div>
+          <p className="text-center text-black dark:text-white">{writer}</p>
+          <p className="text-center text-black dark:text-white">
+            <span className="font-semibold">Price:</span> {price}$
+          </p>
+          <div className="card-actions flex flex-wrap justify-center">
             <button className="btn btn-outline text-black dark:text-white">
               Details
             </button>
@@ -48,9 +61,8 @@ const BookCard = (props: any) => {
               Exchange
             </Link>
             <button className="btn btn-outline text-black dark:text-white">
-              Buy Now
+              Buy
             </button>
-            <button onClick={()=>heandelBookmark(cover, title, writer, price)} className="btn btn-outline">Bookmark</button>
           </div>
         </div>
       </div>
