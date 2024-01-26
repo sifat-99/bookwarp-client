8184bb60f9e7433510bd8582c24cf9821d87ff81
dronatoo <mrdronatoo@gmail.com>
2024-01-26 05:56:35 -0800

    bookcard changed

--- app/Books/BookCard.tsx
+++ app/Books/BookCard.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
+import { url } from "inspector";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
 const BookCard = (props: any) => {
-  const router = useRouter();
 
   const {cover, title, writer, price} = props.books;
   const userInfo=useSession();
@@ -18,28 +18,25 @@ const BookCard = (props: any) => {
         axios.post("https://bookwarp-server.vercel.app/bookmark", bookMark)
         .then(res=>{
           if(res.data.insertedId){
-            toast('Bookmark add successfully');
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Successfully sent message",
+              showConfirmButton: false,
+              timer: 1500
+            })
           }
         })
-      }else{
-        router.push('/login');
       }
-     
   }
   return (
     <div>
-      <div className="card h-[500px] card-compact bg-slate-200 dark:bg-base-100 shadow-xl p-2">
-        <figure>
-          <Image width={250} height={300} src={cover} alt="book" />
-        </figure>
-        <div className="card-body">
-          <h2 className="text-xl text-black dark:text-white text-center font-semibold">
-            {title}
-          </h2>
-          <p className="text-center text-black dark:text-white">{writer}</p>
-          <p className="text-center text-black dark:text-white">
-            <span className="font-semibold">Price:</span> {price}$
-          </p>
+      <div className="card h-[550px] card-compact bg-slate-200 pt-4 dark:bg-black dark:border shadow-xl p-2">
+              <div style={{backgroundImage: `url(${cover})`, background: "cover" }} className="w-[250px] h-[350px]"></div>
+              <div className="card-body">
+                <h2 className="text-xl text-black dark:text-white text-center font-semibold">{title}</h2>
+                <p className='text-center text-black dark:text-white'>{writer}</p>
+                <p className='text-center text-black dark:text-white'><span className='font-semibold'>Price:</span> {price}$</p>
           <div className="card-actions justify-center">
             <button className="btn btn-outline text-black dark:text-white">
               Details
