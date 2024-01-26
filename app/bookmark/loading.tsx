3d618602bbc3daf5e41e58378c994e334137c8e9
dronatoo <mrdronatoo@gmail.com>
2024-01-26 05:28:11 -0800

    added bookmark page

---
+++ app/bookmark/loading.tsx
@@ -0,0 +1,13 @@
+
+
+const loadingPage = () => {
+ 
+    return (
+        <section>
+            <span className="loading loading-spinner loading-lg"></span>
+        </section>
+
+    );
+};
+
+export default loadingPage;    
