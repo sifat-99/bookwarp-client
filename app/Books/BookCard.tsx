8d0ea389908577fa8bc4a64b1614f3992ffdba0e
dronatoo <mrdronatoo@gmail.com>
2024-01-26 04:08:54 -0800

    created detail page

--- app/Books/BookCard.tsx
+++ app/Books/BookCard.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const BookCard = (props:any) => {
-  const {cover, title, writer, price} = props.books;
+  const { _id ,cover, title, writer, price} = props.books;
     return (
         <div>
             <div className="card h-[500px] card-compact bg-slate-200 dark:bg-base-100 shadow-xl p-2">
@@ -12,7 +13,7 @@ const BookCard = (props:any) => {
                 <p className='text-center text-black dark:text-white'>{writer}</p>
                 <p className='text-center text-black dark:text-white'><span className='font-semibold'>Price:</span> {price}$</p>
                 <div className="card-actions justify-center">
-                  <button className="btn btn-outline text-black dark:text-white">Details</button>
+                  <Link href={`allBooks/${_id}`}><button className="btn btn-outline text-black dark:text-white">Details</button></Link>
                   <button className="btn btn-outline text-black dark:text-white">Exchange</button>
                   <button className="btn btn-outline text-black dark:text-white">Buy Now</button>
                 </div>
