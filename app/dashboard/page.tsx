be2a76699b72927e792337d49161cf847dcf1091
Shorif <shorifahamed571@gmail.com>
2024-02-08 00:32:56 +0600

    new dashboard code

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -15,7 +15,6 @@ const Dashboard = () => {
             <div className="me-5 lg:me-0 lg:hidden">
               <div
                 className="flex-none text-xl font-semibold dark:text-white"
-                href="#"
                 aria-label="Brand"
               >
                 Brand
